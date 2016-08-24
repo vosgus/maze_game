432065ad85ab51f0c51dcdf8ae9b82fddfd8e753
Patric Gustafsson <patricjgustafsson@outlook.com>
2016-08-24 21:00:30 +0300

    Add method for pushing stones The player can now push stones.

--- maze.py
+++ maze.py
@@ -15,8 +15,9 @@ def getMaze(width, height, fileName):
             width += 1
     return maze
 
-def getMove():
+def getAction():
     return input()[0]
+        
 
 # Checks if move is legal
 def legalMove(direction, maze, human_position):
@@ -32,6 +33,20 @@ def legalMove(direction, maze, human_position):
         return (maze[human_x][human_y + 1] != '#' and human_y + 1 < 74)
     else:
         return False
+
+def canPush(direction, maze, human_position):
+    human_x, human_y = human_position[0], human_position[1]
+    
+    print(maze[human_x][human_y + 1] + " " + maze[human_x][human_y + 2])
+    
+    if direction == 'w':
+        return (maze[human_x - 1][human_y] == '#' and maze[human_x - 2][human_y] != '#')
+    if direction == 'a':
+        return (maze[human_x][human_y - 1] == '#' and maze[human_x][human_y - 2] != '#')
+    if direction == 's':
+        return (maze[human_x + 1][human_y] == '#' and maze[human_x + 2][human_y] != '#')
+    if direction == 'd':
+        return (maze[human_x][human_y + 1] == '#' and maze[human_x][human_y + 2] != '#')
         
 def drawMaze(maze, width, height):
     for x in range(height):
@@ -47,6 +62,22 @@ def move(direction, human_position):
         human_position[0] += 1
     if direction == 'd':
         human_position[1] += 1
+
+def push(maze, direction, human_position):   
+    human_x, human_y = human_position[0], human_position[1]
+ 
+    if direction == 'w':
+        clearBlock(maze, human_x - 1, human_y)
+        addToBlock(maze, '#', human_x - 2, human_y)
+    if direction == 'a':
+        clearBlock(maze, human_x, human_y - 1)
+        addToBlock(maze, '#', human_x, human_y - 2)
+    if direction == 's':
+        clearBlock(maze, human_x + 1, human_y)
+        addToBlock(maze, '#', human_x + 2, human_y)
+    if direction == 'd':
+        clearBlock(maze, human_x, human_y + 1)
+        addToBlock(maze, '#', human_x, human_y + 2)
         
 def clearBlock(maze, x, y):
     maze[x][y] = ' '
@@ -60,21 +91,28 @@ def clearScreen():
     
 width, height = 74, 23    
 maze = getMaze(width, height, "maze_ascii.txt");
-human = '^'
+human = '&'
 human_position = [0, 0]
 human_position[0], human_position[1] = 1, 1
 maze[human_position[0]][human_position[1]] = human
 
 while (True):
     # First we draw the maze
+    clearScreen()
     drawMaze(maze, width, height);
     
     # Get player to move
-    direction = getMove()
-    if (legalMove(direction, maze, human_position)):
+    action = getAction()
+    
+    if (action in "wasd" and legalMove(action, maze, human_position)):
         clearBlock(maze, human_position[0], human_position[1])
-        move(direction, human_position)
-        addToBlock(maze, human, human_position[0], human_position[1])
+        move(action, human_position)
+        addToBlock(maze, human, human_position[0], human_position[1])        
+    elif (action in "wasd" and canPush(action, maze, human_position)):
+        print("canpush")
+        push(maze, action, human_position)
+        
+        
   
 
        
